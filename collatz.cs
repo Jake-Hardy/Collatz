2db5d32d98549d5696c63a60c2db8bb8b0ae5a4e
Jake-Hardy <jakehrdy@gmail.com>
2015-03-06 12:36:51 -0500

    collatz.cs
    This is a very simple C# program that takes a command line argument, and then tests that value against the Collatz Conjecture. Basically, if a number is even, divide it by 2. If the number is odd, multiply by 3 and add 1. If you continue this pattern, you should eventually get to 1.

---
+++ collatz.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Collatz
+{
+	static void Main(string[] args) 
+	{
+		int startingNum = Int32.Parse(args[0]);
+		int count = 0;
+		
+		Console.WriteLine("Starting Collatz test with " + args[0]);
+		
+		while (startingNum != 1)
+		{
+			if (startingNum % 2 == 0) 
+			{
+				Console.WriteLine(startingNum + " / 2 = " + startingNum/2);
+				startingNum /= 2;
+				count++;
+			}
+			else if (startingNum % 2 == 1)
+			{
+				Console.WriteLine(startingNum + " * 3 + 1 = " + (startingNum * 3 + 1));
+				startingNum = startingNum * 3 + 1;
+				count++;
+			}
+		}
+		
+		Console.WriteLine("Collatz test finished. Took " + count + " iterations to reach 1.");
+	}
+}
